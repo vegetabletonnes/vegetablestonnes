315eb7a6881799df1f1cc634a65af0e7d41ada31
RAMINENI BHANU PRASAD <vegetabletonnes@gmail.com>
2026-08-13 11:27:17 +0530

    2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://vegetabletonnes-api.onrender.com/',
         changeOrigin: true,
         secure: false,
       }
